c19b006254c14508b174533684fdb349bd68262f
Patrick-Jnr Zulu <132206642+jnr-zulu@users.noreply.github.com>
2024-07-07 15:05:26 +0200

    Add files via upload

---
+++ TaxCalculator/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TaxCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(165, 63);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.salaryTextBox.TabIndex = 0;
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(165, 115);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ageTextBox.TabIndex = 1;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(165, 179);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(230, 23);
+            this.CalculateButton.TabIndex = 2;
+            this.CalculateButton.Text = "Calculate Tax";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Salary";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(165, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(314, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "South African Tax Calculator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.salaryTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TaxCalculator/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Code By: Patrick-Jnr Zulu
+        //email: patrickjunior1010@gmail.com
+        //Created to help people calculate their taxes with ease
+
+        // Function to calculate tax payable
+            private double CalculateTax(double salary, int age)
+            {
+                double taxAmount;
+                double rebate;
+
+                // Calculate tax amount based on salary using if-else statements
+                if (salary <= 237100)
+                {
+                    taxAmount = salary * 0.18;
+                }
+                else if (salary <= 370500)
+                {
+                    taxAmount = 42678 + (salary - 237100) * 0.26;
+                }
+                else if (salary <= 512800)
+                {
+                    taxAmount = 77362 + (salary - 370500) * 0.31;
+                }
+                else if (salary <= 673000)
+                {
+                    taxAmount = 115762 + (salary - 512800) * 0.36;
+                }
+                else if (salary <= 857900)
+                {
+                    taxAmount = 173038 + (salary - 673000) * 0.39;
+                }
+                else if (salary <= 1817000)
+                {
+                    taxAmount = 245173 + (salary - 857900) * 0.41;
+                }
+                else
+                {
+                    taxAmount = 636765 + (salary - 1817000) * 0.45;
+                }
+
+                // Calculate rebate based on age
+                if (age < 65)
+                {
+                    rebate = 17235;
+                }
+                else if (age < 75)
+                {
+                    rebate = 17235 + 9444;
+                }
+                else
+                {
+                    rebate = 17235 + 9444 + 3145;
+                }
+
+                // Calculate final tax payable
+                double taxPayable = taxAmount - rebate;
+
+                // Ensure tax payable is not negative
+                return Math.Max(taxPayable, 0);
+            }
+
+            private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            double salary;
+            int age;
+
+            // Get salary input from user
+            if (!double.TryParse(salaryTextBox.Text, out salary) || salary <= 0)
+            {
+                MessageBox.Show("Please enter a valid salary amount.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Get age input from user
+            if (!int.TryParse(ageTextBox.Text, out age) || age <= 0 || age > 120)
+            {
+                MessageBox.Show("Please enter a valid age.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Calculate tax payable using the function
+            double taxPayable = CalculateTax(salary, age);
+
+            // Display result to user
+            MessageBox.Show($"Tax payable for the 2024 tax year: R{taxPayable:N2}", "Tax Calculation Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        
+    }
+    }
+}
